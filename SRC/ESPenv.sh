eaf6e1833e61b1991d149c9ea911b255e24557ee
XuliaDS <docampo@mit.edu>
2019-04-05 08:05:20 -0400

    ESPsource

---
+++ SRC/ESPenv.sh
@@ -0,0 +1,28 @@
+#
+export ESP_ARCH=LINUX64
+export ESP_ROOT=/home/julia/ESP
+export CASROOT=/home/julia/ESP/OpenCASCADE-6.8.1
+export CASARCH=Linux
+export CASREV=6.8
+export PATH=/home/julia/ESP/bin:$PATH
+export PYTHONINC=/usr/include/python2.7
+export PYTHONLIB="-L/usr/lib/python2.7/config-x86_64-linux-gnu -lpython2.7"
+export PYTHONPATH=/home/julia/ESP/lib:$PYTHONPATH
+export EFCOMP=gfortran
+export LD_LIBRARY_PATH=/home/julia/ESP/OpenCASCADE-6.8.1/Linux/lib:/home/julia/ESP/lib:$LD_LIBRARY_PATH
+export UDUNITS2_XML_PATH=/home/julia/ESP/src/CAPS/udunits/udunits2.xml
+#export AFLR=/home/julia/Projects/AFLR
+#export AFLR4=/home/julia/Projects/AFLR
+export AFLR_ARCH=Linux-x86-64
+#export TETGEN=/home/julia/Projects/TetGen/tetgen1.5.0
+#export AWAVE=/home/julia/Projects/awave/awavemod.f
+export SLUGS_START="firefox /home/julia/ESP/SLUGS/Slugs.html"
+export ESP_START="firefox /home/julia/ESP/ESP/ESP.html"
+export WV_START="firefox /home/julia/ESP/wvClient/wv.html"
+
+export ESPFLAGS="-fsanitize=address -fsanitize=undefined -fno-omit-frame-pointer -fuse-ld=gold"
+export IRIT_LIB="/home/julia/IRIT/lib"
+export IRIT_INC="/home/julia/IRIT"
+export IRIT_PATH="/home/julia/IRIT/bin"
+export IRIT_DISPLAY='xgldrvs -s-'
+
